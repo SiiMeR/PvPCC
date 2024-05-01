dc93a38fbff028b5bce683186e61b210c9ee91b1
Siim Raudsepp <siimraudsepp@msn.com>
2024-05-01 12:17:20 +0300

    Implement a slow on hit effect

---
+++ PvPCC/PvPCCModSystem.cs
@@ -0,0 +1,23 @@
+﻿using Vintagestory.API.Client;
+using Vintagestory.API.Server;
+using Vintagestory.API.Config;
+using Vintagestory.API.Common;
+
+namespace PvPCC
+{
+    public class PvPCCModSystem : ModSystem
+    {
+        public override void StartServerSide(ICoreServerAPI api)
+        {
+            api.Event.PlayerNowPlaying += (serverPlayer) =>
+            {
+                if (serverPlayer.Entity is not null)
+                {
+                    var entity = serverPlayer.Entity;
+                    entity.AddBehavior(new SlowOnHitBehavior(entity));
+                    
+                }
+            };
+        }
+    }
+}
---
+++ PvPCC/SlowOnHitBehavior.cs
@@ -0,0 +1,33 @@
+﻿using Vintagestory.API.Common;
+using Vintagestory.API.Common.Entities;
+
+namespace PvPCC;
+
+public class SlowOnHitBehavior : EntityBehavior
+{
+    private const int SlowDurationMilliseconds = 1000;
+    private const float PercentSpeedLossOnHit = 40.0f; // flat
+    public SlowOnHitBehavior(Entity entity) : base(entity)
+    {
+    }
+
+    public override void OnEntityReceiveDamage(DamageSource damageSource, ref float damage)
+    {
+        if (entity is EntityPlayer playerEntity && damageSource.Source == EnumDamageSource.Player)
+        {
+            playerEntity.Stats.Set("walkspeed", "slowonhit", -(PercentSpeedLossOnHit/100.0f));
+            entity.World.RegisterCallback((_) =>
+            {
+                playerEntity.Stats.Remove("walkspeed", "slowonhit");
+            }, SlowDurationMilliseconds);
+        }
+
+
+        base.OnEntityReceiveDamage(damageSource, ref damage);
+    }
+
+    public override string PropertyName()
+    {
+        return "SlowOnHitBehavior";
+    }
+}
